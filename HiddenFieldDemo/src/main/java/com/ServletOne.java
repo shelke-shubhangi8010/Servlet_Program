7201b1485e0a5034adf30da1fb84e9f81f41a3e4
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2026-02-26 14:57:04 +0530

    Hidden Field

---
+++ HiddenFieldDemo/src/main/java/com/ServletOne.java
@@ -0,0 +1,63 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletOne
+ */
+@WebServlet("/ServletOne")
+public class ServletOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletOne() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			
+			response.setContentType("text/html");
+			PrintWriter pw=response.getWriter();
+			String s=request.getParameter("username");
+			pw.print("welcome "+s);
+			
+			pw.print("<form action ='ServletTwo' method='get'>");
+			pw.print("<input type='Hidden' name='hfield' value='"+s+"'>");
+			pw.print("<input type ='submit' value ='visit'>");
+			pw.print("</form>");
+			
+			pw.close();
+			
+		}catch(Exception e) 
+		{
+			System.out.println(e);
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
