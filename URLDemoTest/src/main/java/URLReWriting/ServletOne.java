d0e8d04d32c81e546e0064c275d97e302003413c
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2026-02-26 15:27:06 +0530

    URL ReWriting

---
+++ URLDemoTest/src/main/java/URLReWriting/ServletOne.java
@@ -0,0 +1,58 @@
+package URLReWriting;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletOne
+ */
+@WebServlet("/ServletOne")
+public class ServletOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletOne() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+try {
+			
+			response.setContentType("text/html");
+			PrintWriter pw=response.getWriter();
+			String s=request.getParameter("username");
+			pw.print("welcome "+s);
+			
+			pw.print("<a href ='ServletTwo?uTest="+s+"'> visit</a>");
+			
+			pw.close();
+			
+		}catch(Exception e) 
+		{
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ URLDemoTest/src/main/java/URLReWriting/ServletTwo.java
@@ -0,0 +1,60 @@
+package URLReWriting;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletTwo
+ */
+@WebServlet("/ServletTwo")
+public class ServletTwo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTwo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+try {
+			
+			response.setContentType("text/html");
+			PrintWriter pw=response.getWriter();
+			String s=request.getParameter("uTest");
+			pw.print("Hello "+s);
+			
+			pw.print("This site is under construction");
+			pw.close();
+			
+		}catch(Exception e) 
+		{
+			System.out.println(e);
+		}
+		
+		
+	}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
